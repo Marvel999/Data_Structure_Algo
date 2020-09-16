285a67508aec688d78c506402fc9321d123d687e
Marvel999 <manishjobs1999@gmail.com>
2020-09-17 01:21:27 +0530

    add @D Array Question

---
+++ TwoDimationalArray/intro_two_d.java
@@ -0,0 +1,24 @@
+package TwoDimationalArray;
+
+import java.util.Scanner;
+
+public class intro_two_d {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int[][] arr=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
