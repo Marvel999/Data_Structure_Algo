678809ba99536765ff3dce78cbf124a434966f7c
Marvel999 <manishjobs1999@gmail.com>
2020-09-26 18:18:40 +0530

    add binary tree question

---
+++ Binary_Tree/BinaryTree.java
@@ -0,0 +1,14 @@
+package Binary_Tree;
+
+public class BinaryTree {
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data,Node left,Node right){
+            this.data=data;
+            this.left=left;
+            this.right=right;
+        }
+    }
+}
