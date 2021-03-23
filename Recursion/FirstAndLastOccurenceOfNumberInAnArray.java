bdfce67aea20628bdc4a0b6c4bb7053659f85662
flipxonsoft <manishjobs1999@gmail.com>
2021-03-23 16:49:09 +0530

    [Recursion] add question in recursion list

--- Recursion/FirstAndLastOccurenceOfNumberInAnArray.java
+++ Recursion/FirstAndLastOccurenceOfNumberInAnArray.java
@@ -16,6 +16,26 @@ public class FirstAndLastOccurenceOfNumberInAnArray {
 
     }
 
+    public static int lastOccurence(int[] arr, int n,int i,int key){
+
+        if (i==n){
+            return -1;
+        }
+
+       int b= lastOccurence(arr,n,i+1,key);
+
+        if (b!=-1)
+            return b;
+
+        if (arr[i]==key){
+            return i+1;
+        }
+
+        return -1;
+
+
+    }
+
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int n=scanner.nextInt();
@@ -25,5 +45,6 @@ public class FirstAndLastOccurenceOfNumberInAnArray {
         }
         int key=scanner.nextInt();
         System.out.println(firstOccurence(arr, n,0,key));
+        System.out.println(lastOccurence(arr, n,0,key));
     }
 }
