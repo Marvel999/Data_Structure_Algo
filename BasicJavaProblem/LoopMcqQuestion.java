e4e3e9bed3d75cc5d06fbf4b4d38ca804c10cee8
flipxonsoft <manishjobs1999@gmail.com>
2021-04-04 17:06:42 +0530

    [Feat] Add Question

---
+++ BasicJavaProblem/LoopMcqQuestion.java
@@ -0,0 +1,15 @@
+package BasicJavaProblem;
+
+import java.net.SecureCacheResponse;
+import java.util.Scanner;
+
+public class LoopMcqQuestion {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+//        for (;;){
+//            System.out.println("Loop");
+//        }
+        for (int i=0;i<1;i--)
+            System.out.println("Looop");
+    }
+}
---
+++ BasicJavaProblem/log10Tri.java
@@ -0,0 +1,13 @@
+package BasicJavaProblem;
+
+import java.net.SecureCacheResponse;
+import java.util.Scanner;
+
+public class log10Tri {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int t=10;
+        double out=10.0/0;
+        System.out.println(Math.log10(out));
+    }
+}
