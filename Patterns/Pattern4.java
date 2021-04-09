7742340755e0456cb2473583d803b0e39fe05031
flipxonsoft <manishjobs1999@gmail.com>
2021-04-09 14:49:32 +0530

    [Add Question] Add Question in Patterns Package

--- Patterns/Pattern4.java
+++ Patterns/Pattern4.java
@@ -24,4 +24,24 @@ public class Pattern4 {
 
         }
 
+    public static void mathode() {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n=scn.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i>j){
+                    System.out.print("\t");
+                }else{
+                    System.out.print("*\t");
+                }
+
+            }
+
+            System.out.println();
+        }
+
+    }
+
 }
