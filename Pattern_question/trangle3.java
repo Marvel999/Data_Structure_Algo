c986ee84f64e16ef87b5044a659e619a99e166ff
flipxonsoft <manishjobs1999@gmail.com>
2021-04-11 11:26:40 +0530

    [Add] Add Question in pattern_question package

--- Pattern_question/trangle3.java
+++ Pattern_question/trangle3.java
@@ -18,4 +18,36 @@ public class trangle3 {
             System.out.println();
         }
     }
+
+    public static void trangle1(int n){
+        int spc=n-1;
+        int stc=1;
+        for(int r=1;r<=n;r++){
+
+            for(int i=1;i<=spc;i++){
+                System.out.print("\t");
+            }
+
+            for(int i=1;i<=stc;i++){
+                System.out.print("*\t");
+            }
+            System.out.println();
+            spc--;
+            stc++;
+
+        }
+    }
+    public static void trangle2(int n){
+        for(int c=1;c<=n;c++){
+            for(int r=1;r<=n;r++){
+                if(r+c<n+1)
+                    System.out.print("\t");
+                else
+                    System.out.print("*\t");
+
+
+            }
+            System.out.println();
+        }
+    }
 }
---
+++ String/splitFunctionInString.java
@@ -0,0 +1,7 @@
+package String;
+
+public class splitFunctionInString {
+    public static void main(String[] args) {
+        
+    }
+}
