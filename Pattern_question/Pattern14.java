ee973f73a29c13603f0c0d2582c6f7a6e655a693
flipxonsoft <manishjobs1999@gmail.com>
2021-04-12 17:06:31 +0530

    [Add] Add question in pattern_question

---
+++ Pattern_question/Pattern14.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Pattern14 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        // write ur code here
+
+        int n=scn.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+
+    }
+}
