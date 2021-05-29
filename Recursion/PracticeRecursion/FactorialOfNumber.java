2a65f5833e0d0befbe580bb23979dc7a38a710f0
Marvel999 <manishjobs1999@gmail.com>
2021-05-29 23:53:58 +0530

    [Recursion] Add recursion Question

--- Recursion/PracticeRecursion/FactorialOfNumber.java
+++ Recursion/PracticeRecursion/FactorialOfNumber.java
@@ -1,2 +1,21 @@
-package PracticeRecursion;public class FactorialOfNumber {
+package PracticeRecursion;
+
+import java.util.Scanner;
+
+public class FactorialOfNumber {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        System.out.println(factorial(n));
+    }
+
+    public static int factorial(int n){
+        if(n==0)
+            return 1;
+
+        int fact=n*factorial(n-1);
+        return fact;
+    }
+
 }
