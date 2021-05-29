51bd47cfcbf64dcf4e0fa539157eb6ff5e0b2d33
Marvel999 <manishjobs1999@gmail.com>
2021-05-29 22:33:24 +0530

    [Recursion] Add recursion Question

--- Recursion/PracticeRecursion/PrintIncreasing.java
+++ Recursion/PracticeRecursion/PrintIncreasing.java
@@ -1,4 +1,19 @@
 package PracticeRecursion;
 
+import java.util.Scanner;
+
 public class PrintIncreasing {
+    public static void main(String[] args) throws Exception {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        printIncreasing(n);
+    }
+
+    public static void printIncreasing(int n){
+
+        if(n==0)
+            return;
+        printIncreasing(n-1);
+        System.out.println(n);
+    }
 }
