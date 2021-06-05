da978c6cbad5fd776ec2c13629b28f48eccd5d7a
Marvel999 <manishjobs1999@gmail.com>
2021-06-05 20:50:26 +0530

    [Recursion] Two recursion and Euler diagram

---
+++ Recursion/PracticeRecursion/maximumOfArray.java
@@ -0,0 +1,27 @@
+package PracticeRecursion;
+
+import java.util.Scanner;
+
+public class maximumOfArray {
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scan.nextInt();
+        }
+        System.out.println(maxOfArray(arr,0));
+    }
+
+    public static int maxOfArray(int[] arr, int idx){
+        if(idx==arr.length)
+            return 0;
+
+        int smax=maxOfArray(arr,idx+1);
+        int max =Math.max(arr[idx], smax);
+        return max;
+
+
+    }
+}
