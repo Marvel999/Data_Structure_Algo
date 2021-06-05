9666de85b0a5ea61a1267da102cc7643f3252572
Marvel999 <manishjobs1999@gmail.com>
2021-06-05 21:28:59 +0530

    [Recursion] Two recursion and Euler diagram

--- Recursion/PracticeRecursion/PrintLastIndex.java
+++ Recursion/PracticeRecursion/PrintLastIndex.java
@@ -2,30 +2,30 @@ package PracticeRecursion;
 
 import java.util.Scanner;
 
-public class PrintFirstIndex {
+public class PrintLastIndex {
     public static void main(String[] args) throws Exception {
         // write your code here
         Scanner scan=new Scanner(System.in);
         int n=scan.nextInt();
         int[] arr=new int[n];
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
             arr[i]=scan.nextInt();
-        }
         int x=scan.nextInt();
-        int fi=firstIndex(arr,0,x);
-        System.out.println(fi);
+        int li=lastIndex(arr,0,x);
+        System.out.println(li);
     }
 
-    public static int firstIndex(int[] arr, int idx, int x){
+    public static int lastIndex(int[] arr, int idx, int x){
         if(idx==arr.length)
             return -1;
 
-        if(arr[idx]==x)
+        int lis=lastIndex(arr,idx+1,x);
+        if(lis!=-1)
+            return lis;
+        else if(arr[idx]==x)
             return idx;
-        else{
-            int fis=firstIndex(arr,idx+1,x);
-            return fis;
+        else
+            return -1;
 
-        }
     }
 }
